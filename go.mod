030b67676b479f5a841f3d969c3c2f5665a4d6a6
CAMEL <CAMELNINGA@example.com>
2022-03-15 08:07:54 +0300

    add postgres and update http

--- go.mod
+++ go.mod
@@ -3,6 +3,22 @@ module Yaratam
 go 1.17
 
 require (
+	github.com/beorn7/perks v1.0.1 // indirect
+	github.com/cespare/xxhash/v2 v2.1.2 // indirect
+	github.com/dlmiddlecote/sqlstats v1.0.2 // indirect
+	github.com/golang-migrate/migrate/v4 v4.15.1 // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/hashicorp/errwrap v1.0.0 // indirect
+	github.com/hashicorp/go-multierror v1.1.0 // indirect
 	github.com/jessevdk/go-flags v1.5.0 // indirect
-	golang.org/x/sys v0.0.0-20210320140829-1e4c9ba3b0c4 // indirect
+	github.com/jmoiron/sqlx v1.3.4 // indirect
+	github.com/lib/pq v1.10.0 // indirect
+	github.com/matttproud/golang_protobuf_extensions v1.0.2-0.20181231171920-c182affec369 // indirect
+	github.com/prometheus/client_golang v1.12.1 // indirect
+	github.com/prometheus/client_model v0.2.0 // indirect
+	github.com/prometheus/common v0.32.1 // indirect
+	github.com/prometheus/procfs v0.7.3 // indirect
+	go.uber.org/atomic v1.6.0 // indirect
+	golang.org/x/sys v0.0.0-20220114195835-da31bd327af9 // indirect
+	google.golang.org/protobuf v1.27.1 // indirect
 )
---
+++ internal/infra/postgres/adapter.go
@@ -0,0 +1,60 @@
+package postgres
+
+import (
+	"Yaratam/internal/domain"
+	"errors"
+	"github.com/dlmiddlecote/sqlstats"
+	"github.com/golang-migrate/migrate/v4"
+	postgres "github.com/golang-migrate/migrate/v4/database/postgres"
+	"github.com/jmoiron/sqlx"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/sirupsen/logrus"
+)
+
+type adapter struct {
+	logger logrus.FieldLogger
+	config *Config
+	db     *sqlx.DB
+}
+
+func NewAdapter(logger logrus.FieldLogger, config *Config) (domain.Database, error) {
+	a := &adapter{
+		logger: logger,
+		config: config,
+	}
+
+	db, err := sqlx.Open("pgx", config.ConnectionString())
+	if err != nil {
+		logger.Errorf("cannot open sql connection: %w", err)
+		return nil, err
+	}
+	a.db = db
+
+	// Create a new collector, the name will be used as a label on the metrics
+	collector := sqlstats.NewStatsCollector("credit_history_db", db)
+
+	// Register it with Prometheus
+	prometheus.MustRegister(collector)
+
+	db.SetMaxOpenConns(config.MaxOpenConns)
+	db.SetMaxIdleConns(config.MaxIdleConns)
+	db.SetConnMaxLifetime(config.ConnMaxLifeTime)
+
+	// Migrations block
+	driver, err := postgres.WithInstance(db.DB, &postgres.Config{})
+	if err != nil {
+		return nil, err
+	}
+
+	m, err := migrate.NewWithDatabaseInstance(config.MigrationsSourceURL, config.Name, driver)
+	if err != nil {
+		return nil, err
+	}
+
+	err = m.Up()
+	if err != nil && !errors.Is(err, migrate.ErrNoChange) {
+		return nil, err
+	}
+
+	return a, nil
+}
